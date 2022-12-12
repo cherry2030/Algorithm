ebf9ee539131a699af4c1edd5752e697af6b1326
cherry2030 <64332359+cherry2030@users.noreply.github.com>
2022-12-12 12:04:41 +0900

    solution 18 - method 2

--- solution11_20/Solution18.java
+++ solution11_20/Solution18.java
@@ -9,7 +9,6 @@ public class Solution18 {
             return 0;
         }
         str = str.replaceAll(" ", "");
-        System.out.println(str);
         for(int i=0;i<str.length();i++){
             if(str.charAt(i) <='9' && str.charAt(i)>='1'){ // 숫자인 경우
                 total += Character.getNumericValue(str.charAt(i));
@@ -17,6 +16,24 @@ public class Solution18 {
             }
         }
         return (int) Math.round(total/(str.length()-cnt));
+    }
+
+    // 방법 2
+    public static int numberSearch2(String str) {
+        String num = "0123456789";
+        double total=0;
+        int cnt=0;
+        if(str.length()==0) return 0;
+
+        for(int i=0;i<str.length();i++){
+            if(num.indexOf(str.charAt(i)) != -1){ // 숫자인 경우
+                total += Character.getNumericValue(str.charAt(i));
+            } else if (str.charAt(i) != ' ') {// 숫자, 공백을 제외한 길이 세기
+                cnt += 1;
+            }
+        }
+
+        return (int) Math.round((total/cnt));
     }
 
     public static void main(String[] args) {
