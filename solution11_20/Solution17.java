b77c6d45adb1a4fe13ca663081f2017925808951
cherry2030 <64332359+cherry2030@users.noreply.github.com>
2022-12-12 19:28:18 +0900

    solution 17 - method 2

--- solution11_20/Solution17.java
+++ solution11_20/Solution17.java
@@ -5,29 +5,38 @@ public class Solution17 {
     // sqrt 사용 금지
 
     // 방법 1
-//    public static String computeSquareRoot(int num) {
-//        double sqrtNum = 1;
-//        double[] diffs = new double[]{1, 0.1, 0.01, 0.001};
-//
-//        for (int i = 0; i < diffs.length; i++) {
-//            while (sqrtNum * sqrtNum < num) {
-//                sqrtNum += diffs[i];
-//                if (sqrtNum * sqrtNum == num) {
-//                    return String.format("%.2f", sqrtNum);
-//                }
-//                if (sqrtNum * sqrtNum > num) {
-//                    sqrtNum -= diffs[i];
-//                    break;
-//                }
-//            }
-//        }
-//
-//        return String.format("%.2f", sqrtNum);
-//    }
+    public static String computeSquareRoot(int num) {
+        double sqrtNum = 1;
+        double[] diffs = new double[]{1, 0.1, 0.01, 0.001};
+
+        for (int i = 0; i < diffs.length; i++) {
+            while (sqrtNum * sqrtNum < num) {
+                sqrtNum += diffs[i];
+            }
+            if (sqrtNum * sqrtNum == num) {
+                return String.format("%.2f", sqrtNum);
+            }
+            if (sqrtNum * sqrtNum > num) {
+                sqrtNum -= diffs[i];
+            }
+        }
+
+        return String.format("%.2f", sqrtNum);
+    }
 
     // 방법 2 - 바빌로니아 법 사용
-    public static String computeSquareRoot(int num) {
-
+    // 바빌로니아 법은 x가 무한으로 갈수록, 점차 값에 수렴한다는 것!
+    // 하지만 우리는 무한 루프를 돌 수 없기 때문에, 정밀도를 설정해야 한다.
+    public static String computeSquareRoot2(int num) {
+        double x = 1;
+        while( x*x != num){
+            if(x*x == num){
+                break;
+            }
+            //
+            x = (x + (num/x)) /2;
+        }
+        return String.format("%.2f", x);
     }
 
     public static void main(String[] args) {
