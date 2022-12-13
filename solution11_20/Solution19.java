262367a95e1313444e0a3bfc64b1ad7b221a015d
cherry2030 <64332359+cherry2030@users.noreply.github.com>
2022-12-13 11:31:05 +0900

    solution19 - method 2

--- solution11_20/Solution19.java
+++ solution11_20/Solution19.java
@@ -1,5 +1,7 @@
 package daily_coding.solution11_20;
 
+import java.awt.desktop.AppForegroundListener;
+
 public class Solution19 {
     // 방법 1
     public static String decryptCaesarCipher(String str, int secret) {
@@ -20,6 +22,25 @@ public class Solution19 {
         }
         return result;
     }
+    public static String decryptCaesarCipher2(String str, int secret) {
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        String result="";
+        if(str.length()==0) return result;
+
+        // 복호화 - secret 만큼 앞당져줘야 한다.
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == ' '){
+                result += " ";
+                continue;
+            }
+            int as_is = alpha.indexOf(str.charAt(i)); // 현재 위치
+            // index를 벗어날 경우에 순환
+            int to_be = (as_is - secret + alpha.length()) % alpha.length();
+            result += alpha.charAt(to_be);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         // a b c d e f g h i j k l m n o p q r s t u v w x y z
         String output = decryptCaesarCipher("khoor", 3);
@@ -28,8 +49,8 @@ public class Solution19 {
         output = decryptCaesarCipher("zruog", 3);
         System.out.println(output); // --> world
 
-        output = decryptCaesarCipher("nzop delepd dfaazced jzf", 11);
-        System.out.println(output);
+        String output2 = decryptCaesarCipher2("nzop delepd dfaazced jzf", 11);
+        System.out.println(output2);
 
     }
 
