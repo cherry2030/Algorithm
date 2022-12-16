e7256e557a6111752cf2c2136596e3b4da0a1d0e
cherry2030 <64332359+cherry2030@users.noreply.github.com>
2022-12-16 20:27:10 +0900

    solution20 - method 2

--- solution11_20/Solution20.java
+++ solution11_20/Solution20.java
@@ -1,6 +1,7 @@
 package daily_coding.solution11_20;
 
 public class Solution20 {
+    // 방법 1
     public static String compressString(String str) {
         int cnt;
         String result = "";
@@ -22,11 +23,38 @@ public class Solution20 {
         return result;
     }
 
+    // 방법 2
+    public static String compressString2(String str) {
+        char before = str.charAt(0);
+        int cnt = 1;
+        String result = "";
+
+        str+= " ";
+        for(int i=1;i<str.length();i++){
+            if(before == str.charAt(i)){
+                cnt+=1;
+            }
+            else{ // 다른 문자를 마주친다면
+                if(cnt>=3){
+                    result += cnt;
+                    result += before;
+                }
+                else{
+                    result+= String.valueOf(before).repeat(cnt);
+                }
+                before = str.charAt(i);
+                cnt=1;
+            }
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
-        String output = compressString("abc");
+        String output = compressString2("abc");
         System.out.println(output); // --> "abc"
 
-        output = compressString("wwwggoppopppp");
+        output = compressString2("dsafdasfffffsgaaaaabbbscccdd");
         System.out.println(output); // --> "3wggoppo4p"
     }
 }
