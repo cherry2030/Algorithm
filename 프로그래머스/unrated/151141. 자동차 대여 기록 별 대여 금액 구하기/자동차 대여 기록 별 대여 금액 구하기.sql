1efd984bd7fc9d5746ce05d80d3f553cacb2f906
ChaeEun <64332359+cherry2030@users.noreply.github.com>
2023-06-24 11:44:56 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -2,36 +2,20 @@
 -- 자동차 종류가 '트럭'인 자동차
 -- 대여 기록 별로 대여 금액(컬럼명: FEE)을 구하여 대여 기록 ID와 대여 금액 리스트를 출력하는 SQL문
 -- 대여 금액을 기준으로 내림차순 정렬하고, 대여 기록 ID를 기준으로 내림차순 정렬
+-- 
+-- order by FEE desc, HISTORY_ID desc
 
-/*SELECT HISTORY_ID, 
-round(DAILY_FEE * (DATEDIFF(END_DATE, START_DATE)+1)* (CASE WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 90 then 0.9
-                                                    when DATEDIFF(END_DATE, START_DATE)+1 >= 30 then 0.93
-                                                    when DATEDIFF(END_DATE, START_DATE)+1 >= 7 then 0.95
-                                                    else 1 end)) as FEE
+with A as (select HISTORY_ID, H.CAR_ID, START_DATE, END_DATE, CAR_TYPE, DAILY_FEE, 
+           (CASE WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 90 then "90일 이상"
+                    when DATEDIFF(END_DATE, START_DATE)+1 >= 30 then "30일 이상"
+                    when DATEDIFF(END_DATE, START_DATE)+1 >= 7 then "7일 이상"
+                    else "NULL" end) as duration 
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY H
 join CAR_RENTAL_COMPANY_CAR C on H.CAR_ID = C.CAR_ID
-join CAR_RENTAL_COMPANY_DISCOUNT_PLAN P on P.CAR_TYPE = C.CAR_TYPE
-where C.CAR_TYPE = '트럭'
-group by HISTORY_ID
+where C.CAR_TYPE = '트럭')
+
+
+SELECT A.HISTORY_ID, round(DAILY_FEE * (DATEDIFF(END_DATE, START_DATE)+1)* (100 - IFNULL(DISCOUNT_RATE , 0)) / 100) as FEE
+from A
+left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN P on (P.CAR_TYPE = A.CAR_TYPE and P.DURATION_TYPE = A.duration)
 order by FEE desc, HISTORY_ID desc
-*/
-
-SELECT HISTORY_ID, 
-    round(DAILY_FEE * (DATEDIFF(h.END_DATE,h.START_DATE)+1)
-    * (case 
-    when DATEDIFF(END_DATE,START_DATE)+1 < 7 then 1
-    when DATEDIFF(END_DATE,START_DATE)+1 < 30 then 0.95
-    when DATEDIFF(END_DATE,START_DATE)+1 < 90 then 0.92
-    else 0.85 end)) as "FEE"
-
-from CAR_RENTAL_COMPANY_CAR as c 
-    join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
-    on c.CAR_ID = h.CAR_ID
-    join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
-    on c.CAR_TYPE = p.CAR_TYPE
-
-where c.car_type = "트럭"
-
-group by HISTORY_ID
-
-order by FEE desc , HISTORY_ID desc
